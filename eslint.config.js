76226aff58e3e98d4299acae8e412f8061bbf9aa
lchanss <lchans0319@gmail.com>
2025-11-28 23:22:59 +0900

    feat: eslint 세부 설정

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'vite.config.ts']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -18,6 +19,44 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+     plugins: {
+      import: importPlugin,
+    },
+     rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          disallowTypeAnnotations: false,
+        },
+      ],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external", // 외부 패키지
+            "internal", // @/ 절대경로
+            ["parent", "sibling", "index"], // 상대경로
+          ],
+          pathGroups: [
+            { pattern: "{react,react-*,react-*/**}", group: "builtin", position: "before" }, // react는 builtin으로 별도 지정
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+          "newlines-between": "always-and-inside-groups",
+          pathGroupsExcludedImportTypes: ["react"], // react는 external에서 제외
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   },
 ])
