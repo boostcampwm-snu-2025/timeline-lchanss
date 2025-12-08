fc16e1c856e9953bd5676b6f700379af45e00cc2
lchanss <lchans0319@gmail.com>
2025-12-08 22:24:12 +0900

    fix: 프리티어 적용되지 않는 문제 해결
    - .prettierrc에서 plugins 속성 제거하여 해결
    - 이에 따라 빈파일이 되었으므로 파일 자체를 삭제

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-    return (
-      <div>timeline project</div>
-  )
+  return <div>timeline project</div>;
 }
 
-export default App
+export default App;
