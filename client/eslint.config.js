32f0b19a4caab3abe62e7450b966998391bb4b82
lchanss <lchans0319@gmail.com>
2025-12-14 16:43:44 +0900

    remove: react-zoom-pan-pinch 패키지 제거

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,16 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 import importPlugin from "eslint-plugin-import";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default defineConfig([
-  globalIgnores(['dist', 'vite.config.ts']),
+  globalIgnores(["dist", "vite.config.ts"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -21,11 +21,11 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-     plugins: {
+    plugins: {
       import: importPlugin,
       "@tanstack/query": pluginQuery,
     },
-     rules: {
+    rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
@@ -44,7 +44,11 @@ export default defineConfig([
             ["parent", "sibling", "index"], // 상대경로
           ],
           pathGroups: [
-            { pattern: "{react,react-*,react-*/**}", group: "builtin", position: "before" }, // react는 builtin으로 별도 지정
+            {
+              pattern: "{react,react-*,react-*/**}",
+              group: "builtin",
+              position: "before",
+            }, // react는 builtin으로 별도 지정
             {
               pattern: "@/**",
               group: "internal",
@@ -61,4 +65,4 @@ export default defineConfig([
       ],
     },
   },
-])
+]);
